19963b0aef0253f0ad9c39f75954f79397ea8fde
vdtdg <valerian@dtdg.fr>
2025-02-04 00:03:34 +0100

    kata 003

--- kata-003/script.js
+++ kata-003/script.js
@@ -3,7 +3,6 @@
 const list = document.querySelector("#myList");
 const items = list.querySelectorAll("li");
 
-// Modifie le texte de chaque élément de la liste
 for (let i = 0; i < items.length; i++) {
   items[i].textContent += " – modifié";
 }
